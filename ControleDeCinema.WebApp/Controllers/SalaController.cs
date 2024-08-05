347bce5a967d372745553653274e732c1afde060
LeoDallago <leodallago@outlook.com>
2024-08-05 09:27:55 -0300

    implementado interface inserir sala

---
+++ ControleDeCinema.WebApp/Controllers/SalaController.cs
@@ -0,0 +1,35 @@
+﻿using ControleDeCinema.Dominio.ModuloSala;
+using ControleDeCinema.Infra.Orm.Compartilhado;
+using ControleDeCinema.Infra.Orm.ModuloSala;
+using ControleDeCinema.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeCinema.WebApp.Controllers;
+
+public class SalaController : Controller
+{
+    [HttpGet]
+    public ViewResult Inserir()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ViewResult Inserir(SalaViewModels.InserirSalaViewModel inserirSalaViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(inserirSalaViewModel);
+        }
+
+        var db = new ControleDeCinamaDbContext();
+        var repositorioSala = new RepositorioSala(db);
+
+        var novaSala = new Sala(inserirSalaViewModel.Numero, inserirSalaViewModel.Capacidade);
+        
+        repositorioSala.Inserir(novaSala);
+
+        ViewBag.Mensagem = $"A sala {novaSala.Numero} foi inserida com Sucesso!";
+        return View("mensagens");
+    }
+}
---
+++ ControleDeCinema.WebApp/Models/SalaViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeCinema.WebApp.Models;
+
+public class SalaViewModels
+{
+    public class InserirSalaViewModel
+    {
+        [Required(ErrorMessage = "Por favor, insira um Numero")]
+        public string Numero { get; set; }
+        
+        [Required(ErrorMessage = "Por favor, insira a CAPACIDADE")]
+        public int Capacidade { get; set; }
+    }
+}
